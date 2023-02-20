c388e3015d11ed26538814366469bbf75ea0cbea
nejlabykrdll <nejlabykrdl@gmail.com>
2023-02-20 11:55:58 +0300

    stack

---
+++ FUNCTIONS/stack.js
@@ -0,0 +1,5 @@
+let number1=10;
+let number2=number1
+number1=20
+console.log(number1)
+console.log(number2)
---
+++ FUNCTIONS/stackobject.js
@@ -0,0 +1,9 @@
+let object1={
+    name: 'emre',
+    id: 1,
+};
+let object2=object1
+object1.id=100
+object1.name='mert'
+console.log(object1)
+console.log(object2)
